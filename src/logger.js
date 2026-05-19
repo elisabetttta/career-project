26494af981aecae5648375e500637e818a35b966
Елизавета <safonova.elisaveta@mail.ru>
2026-05-20 00:47:09 +0300

    Файлы распределены по папкам src и tests согласно принципам SoC

---
+++ src/logger.js
@@ -0,0 +1,19 @@
+const config = require('../config');
+
+const logger = {
+    log(level, message) {
+        const timestamp = new Date().toISOString();
+        // Простая авторская генерация уникального ID для контекста строки лога
+        const requestId = Math.random().toString(36).substring(2, 10).toUpperCase();
+        
+        console.log("[" + timestamp + "] [" + config.appName + "] [" + level.toUpperCase() + "] [ReqID: " + requestId + "]: " + message);
+    },
+
+    error(message) { this.log('error', message); },
+    warn(message) { this.log('warn', message); },
+    info(message) { this.log('info', message); },
+    debug(message) { this.log('debug', message); },
+    trace(message) { this.log('trace', message); }
+};
+
+module.exports = logger;
---
+++ tests/logger.test.js
@@ -0,0 +1,18 @@
+const logger = require('../src/logger');
+
+describe('Logger Tests', () => {
+    beforeEach(() => {
+        jest.spyOn(console, 'log').mockImplementation(() => {});
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    test('should log message with INFO level', () => {
+        logger.info('Test info message');
+        expect(console.log).toHaveBeenCalledWith(
+            expect.stringContaining('[INFO]')
+        );
+    });
+});

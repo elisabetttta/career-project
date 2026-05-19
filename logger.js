7e4d0c8a549a0cc806458ffa946fa862066cc12d
Елизавета <safonova.elisaveta@mail.ru>
2026-05-20 00:14:31 +0300

    Обновлен логгер, добавлен контекст requestId

--- logger.js
+++ logger.js
@@ -3,12 +3,17 @@ const config = require('./config');
 const logger = {
     log(level, message) {
         const timestamp = new Date().toISOString();
-        console.log("[" + timestamp + "] [" + config.appName + "] [" + level.toUpperCase() + "]: " + message);
+        // Простая авторская генерация уникального ID для контекста строки лога
+        const requestId = Math.random().toString(36).substring(2, 10).toUpperCase();
+        
+        console.log("[" + timestamp + "] [" + config.appName + "] [" + level.toUpperCase() + "] [ReqID: " + requestId + "]: " + message);
     },
 
-    info(message) { this.log('info', message); },
+    error(message) { this.log('error', message); },
     warn(message) { this.log('warn', message); },
-    error(message) { this.log('error', message); }
+    info(message) { this.log('info', message); },
+    debug(message) { this.log('debug', message); },
+    trace(message) { this.log('trace', message); }
 };
 
 module.exports = logger;

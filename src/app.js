2e61b8f6f3b11fccdb28e201134618b64a107f28
Елизавета <safonova.elisaveta@mail.ru>
2026-05-28 01:25:01 +0300

    Добавлен post /currencies

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ const express = require("express");
 const authMiddleware = require("./authMiddleware");
 const app = express();
 app.use(express.json());
+const currencies = [];
 app.get("/status", (req, res) => {
   res.send("ok");
 });
 app.get("/secret", authMiddleware, (req, res) => {
   res.send("you have access");
+});
+app.get("/currencies", authMiddleware, (req, res) => {
+  res.json(currencies);
+  });
+  app.post("/currencies", authMiddleware, (req, res) => {
+  const { name, ticker } = req.body;
+
+  const newCurrency = {
+    name,
+    ticker
+  };
+
+  currencies.push(newCurrency);
+
+  res.status(201).json(newCurrency);
 });
 module.exports = app;
